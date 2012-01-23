c4e81364d896bb057e30fa568c034e6e931397df
Daniel Watzinger <daniel.watzinger@gmail.com>
2012-01-23 16:51:53 +0100

    upsampling

--- Projekt 3/code/src/main/java/net/semanticmetadata/lire/imageanalysis/mpeg7/ColorStructureDescriptorImplementation.java
+++ Projekt 3/code/src/main/java/net/semanticmetadata/lire/imageanalysis/mpeg7/ColorStructureDescriptorImplementation.java
@@ -16,7 +16,7 @@ public class ColorStructureDescriptorImplementation {
 	public static final int[] HUE32_QUANT = { 1, 4, 4, 4 };
 	public static final int[] SUM32_QUANT = { 8, 4, 1, 1 };
 	public static final int HUE_MAX_VALUE = 360, RGB_MAX_VALUE = 255;
-	public static final int  BIN256 = 256, BIN128 = 128, BIN64 = 64, BIN32 = 32;
+	public static final int BIN256 = 256, BIN128 = 128, BIN64 = 64, BIN32 = 32;
 	public static final int STRUCT_ELEM_SIZE = 8;
 
 	public static float[] extractCSD(BufferedImage img, int binnum) {
@@ -27,26 +27,48 @@ public class ColorStructureDescriptorImplementation {
 			throw new IllegalArgumentException("Wrong number of bins");
 
 		int width = img.getWidth(), height = img.getHeight();
+
+		if (width == 0 || height == 0)
+			throw new IllegalArgumentException();
+
 		int[] rgb = new int[height * width];
 		img.getRGB(0, 0, width, height, rgb, 0, width);
 
 		/*
-		 * TODO: upsample images smaller than 8x8 by the smallest power of 2 (in
-		 * both directions) such that the minimum of the width and height of the
+		 * Upsample images smaller than 8x8 by the smallest power of 2 (in both
+		 * directions) such that the minimum of the width and height of the
 		 * resulting image is greater than or equal to 8
 		 */
 
 		if (width < 8 || height < 8) {
+			int nWidth = width, nHeight = height;
+			int upFactor = 1;
 
+			while (nWidth < 8 || nHeight < 8) {
+				nWidth *= 2;
+				nHeight *= 2;
+				upFactor *= 2;
+			}
+
+			int[] nRGB = new int[nHeight * nWidth];
+
+			for (int y = 0; y < height; y++) {
+				for (int x = 0; x < width; x++) {
+
+					for (int yy = 0; yy < upFactor; yy++) {
+						for (int xx = 0; xx < upFactor; xx++) {
+							int idx = (y * upFactor + yy) * nWidth + x
+									* upFactor + xx;
+							nRGB[idx] = rgb[y * width + x];
+						}
+					}
+				}
+			}
+
+			rgb = nRGB;
+			width = nWidth;
+			height = nHeight;
 		}
-
-		/* Convert to HMMD color space */
-//		float[][] hmmd = new float[width * height][5];
-//
-//		for (int i = 0; i < rgb.length; i++) {
-//			hmmd[i] = HMMD.rgb2hmmd((rgb[i] >> 16) & 0xFF,
-//					(rgb[i] >> 8) & 0xFF, rgb[i] & 0xFF);
-//		}
 
 		/* Determine spatial extent of structuring element */
 		long p, k, e;
@@ -58,83 +80,86 @@ public class ColorStructureDescriptorImplementation {
 			k = (long) Math.pow(2, p);
 			e = 8 * k;
 		}
-		
-		System.out.println("Width "+width+" Height = "+height);
-		System.out.println("K = "+k+" E = "+e);
 
 		/*
 		 * Accumulate CS histogram by sliding the structuring element across the
 		 * image. The stride size is determined by the subsampling factor
 		 */
 		float[] csd = new float[binnum];
-		
-		for (int y = 0; y < height-STRUCT_ELEM_SIZE+1; y += k) {
-			for (int x = 0; x < width-STRUCT_ELEM_SIZE+1; x += k) {
+
+		for (int y = 0; y < height - STRUCT_ELEM_SIZE + 1; y += k) {
+			for (int x = 0; x < width - STRUCT_ELEM_SIZE + 1; x += k) {
 				int[] tmp = new int[binnum];
-				
+
 				/* Traverse structuring element */
-				for (int yy = y; yy < y+ STRUCT_ELEM_SIZE *k; yy+=k) {
-					for (int xx = x; xx < x+STRUCT_ELEM_SIZE*k; xx+=k) {
-						
-						int r = 0, g=0, b=0;
-						
+				for (int yy = y; yy < y + STRUCT_ELEM_SIZE * k; yy += k) {
+					for (int xx = x; xx < x + STRUCT_ELEM_SIZE * k; xx += k) {
+
+						int r = 0, g = 0, b = 0;
+
 						/* Subsampling */
-						for (int i = yy; i < yy+k; i++) {
-							for (int j = xx; j < xx+k; j++) {
-								int idx = i*width + j;
+						for (int i = yy; i < yy + k; i++) {
+							for (int j = xx; j < xx + k; j++) {
+								int idx = i * width + j;
 								r += (rgb[idx] >> 16) & 0xFF;
 								g += (rgb[idx] >> 8) & 0xFF;
 								b += rgb[idx] & 0xFF;
 							}
 						}
-						
-						r /= k*k; g /= k*k; b/=k*k;
-						//System.out.println("Red: "+r+ " Green: "+g+" Blue: "+b);
-						
+
+						r /= k * k;
+						g /= k * k;
+						b /= k * k;
+						// System.out.println("Red: "+r+
+						// " Green: "+g+" Blue: "+b);
+
 						/* Convert to HMMD and increment bin */
 						float[] hmmd = HMMD.rgb2hmmd(r, g, b);
-						tmp[binIndex(hmmd, binnum)]+=1;
+						tmp[binIndex(hmmd, binnum)] += 1;
 					}
 				}
-				
+
 				/* Increment color structure descriptor bins */
 				for (int i = 0; i < tmp.length; i++) {
 					if (tmp[i] > 0)
-						csd[i]+=1;
+						csd[i] += 1;
 				}
 			}
 		}
-		
+
 		/* Normalize color structure bin values */
 		float normFac = (width / k - 7) * (height / k - 7);
 		for (int i = 0; i < csd.length; i++) {
 			csd[i] /= normFac;
 		}
-		
+
 		return csd;
 	}
 
 	private static double log2(double x) {
 		return Math.log(x) / Math.log(2.0f);
 	}
-	
+
 	public static float distance(float[] first, float[] second) {
-		if (first.length < second.length) second = requant(second, first.length);
-		else first = requant(first, second.length);
-		
+		if (first.length < second.length)
+			second = requant(second, first.length);
+		else
+			first = requant(first, second.length);
+
 		float distance = 0;
-		
+
 		for (int i = 0; i < first.length; i++) {
 			distance += Math.abs(first[i] - second[i]);
 		}
-		
-		return distance /= first.length;	
+
+		return distance /= first.length;
 	}
-	
+
 	public static float[] requant(float[] csd, int binsize) {
-		if((binsize != BIN256 && binsize != BIN128 && binsize != BIN64 && binsize != BIN32) || binsize > csd.length)
+		if ((binsize != BIN256 && binsize != BIN128 && binsize != BIN64 && binsize != BIN32)
+				|| binsize > csd.length)
 			throw new IllegalArgumentException();
-		
+
 		/* TODO: implement requantization */
 		return csd;
 	}
@@ -165,7 +190,8 @@ public class ColorStructureDescriptorImplementation {
 					+ (int) ((hue / HUE_MAX_VALUE) * HUE32_QUANT[subspace]);
 		}
 
-		//System.out.println("Hue: "+ hue+" Sum: "+sum +" subspace: "+subspace+" Index: "+index);
+		// System.out.println("Hue: "+ hue+" Sum: "+sum
+		// +" subspace: "+subspace+" Index: "+index);
 		return index;
 	}
 
@@ -250,16 +276,22 @@ public class ColorStructureDescriptorImplementation {
 	}
 
 	public static void main(String args[]) throws Exception {
-		BufferedImage img1 = ImageIO.read(new File("image.orig/5.jpg"));	
+		BufferedImage img1 = ImageIO.read(new File("image.orig/5.jpg"));
 		BufferedImage img2 = ImageIO.read(new File("image.orig/4.jpg"));
-		float[] csd1 = ColorStructureDescriptorImplementation.extractCSD(img1, 32);
-		float[] csd2 = ColorStructureDescriptorImplementation.extractCSD(img2, 32);
-		
-		for (int i = 0; i < csd1.length; i++) {
-			System.out.print(csd1[i]+" ");
+		BufferedImage small = ImageIO.read(new File("image.orig/small.png"));
+		float[] csd1 = ColorStructureDescriptorImplementation.extractCSD(img1,
+				256);
+		float[] csd2 = ColorStructureDescriptorImplementation.extractCSD(img2,
+				256);
+		float[] csd3 = ColorStructureDescriptorImplementation.extractCSD(small,
+				32);
+
+		for (int i = 0; i < csd3.length; i++) {
+			System.out.print(csd3[i] + " ");
 		}
 		System.out.println();
-		System.out.println(ColorStructureDescriptorImplementation.distance(csd1, csd2));
+		System.out.println(ColorStructureDescriptorImplementation.distance(
+				csd1, csd2));
 
 		// for (int i = 0; i < hmmd.length; i++)
 		// System.out.println(hmmd[i]);
