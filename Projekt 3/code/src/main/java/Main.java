23bd94fd67066801fa47e556844a5aa756fc66f6
Bastian Huber <huber.baste@googlemail.com>
2012-01-30 20:37:57 +0100

    fix

--- Projekt 3/code/src/main/java/Main.java
+++ Projekt 3/code/src/main/java/Main.java
@@ -17,12 +17,10 @@ public class Main {
 	public static void main(String[] args) {
 		File qbe = new File("image.orig/1.jpg");
 		Set<Integer> best = new HashSet<Integer>(getLinkedList(new int[] { 0,
-				1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 18, 19, 20, 21,
-				22, 23, 24, 25, 26, 27, 29, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-				41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 52, 53, 54, 55, 56, 57,
-				58, 59, 60, 61, 62, 64, 65, 66, 67, 68, 69, 70, 71, 74, 75, 76,
-				77, 78, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 91, 92, 93, 94,
-				95, 96, 97, 98, 99 }));
+				1, 7, 8, 9, 10, 11, 12, 15, 16, 18, 20, 21, 23, 24, 25, 26, 27,
+				29, 32, 34, 35, 36, 37, 38, 39, 41, 42, 43, 44, 47, 49, 53, 57,
+				59, 60, 61, 62, 64, 65, 66, 67, 68, 71, 74, 75, 76, 92, 96, 97,
+				98 }));
 
 		Model model = new Model();
 		int step = 15;
@@ -60,7 +58,7 @@ public class Main {
 				/*
 				 * Recall
 				 */
-				float recall = foundRelevant.size() / best.size();
+				float recall = foundRelevant.size() / (float) best.size();
 
 				System.out.printf("%.4f & %.4f", precision, recall);
 
